04ba2e65e1579b73e5c37b56956223e3d0d005a9
Kosmo00 <vladiotero19101916@gmail.com>
2021-08-16 02:04:33 -0400

    All practices pages migrated

---
+++ pages/practices/vproblems/index.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+function VProblems(): JSX.Element {
+  return (
+    <>
+      <h2 className="postheader">
+        {/* <a class="linkheader" href="<c:url value="vcontestview.xhtml?cid=${contest.cid}"/>">${contest.name}</a> */}
+        <br />
+        spring:message code="pagehdr.problems"
+      </h2>
+      <div className="postcontent">
+        <div
+          id="display-table-container"
+          data-reload-url="/tables/vproblems.xhtml"
+        ></div>
+      </div>
+      {/* <script>
+            $(document).ready(displayTableReload(" "));
+          </script> */}
+    </>
+  )
+}
+
+export default VProblems
