eafb41436328a8a58d5850de4aac834d88c350e9
Jesus Reikel <68043863+jesusr00@users.noreply.github.com>
2021-08-15 22:40:41 -0400

    bootstrap implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import "../styles/blueimp-gallery-indicator.css"
 import "../styles/globals.css"
 import "../styles/main.css"
 import "../styles/blueimp-gallery-video.css"
-import "../styles/bootstrap-dialog.min.css"
 import "../styles/confirm-message.css"
 import "../styles/custom.bootstrap.tooltip.css"
 import "../styles/datatable.css"
@@ -17,10 +16,14 @@ import "../styles/style.css"
 import "../styles/TimeCircles.css"
 import "../styles/Home.module.css"
 import "../styles/wboard.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/css/bootstrap-grid.css"
+import "../node_modules/bootstrap/dist/css/bootstrap-reboot.css"
+import "../node_modules/bootstrap/dist/css/bootstrap-utilities.css"
 
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+   return <Component {...pageProps} />
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,110 +5,115 @@ import Footer from "../components/Footer"
 import Sidebar from "../components/Sidebar"
 
 export default function Home(): JSX.Element {
-  return (
-    <>
-      <div id="main">
-        <div className="sheet">
-          <div className="sheet-body">
-            <Header />
+   return (
+      <>
+         <div id="main">
+            <div className="sheet">
+               <div className="sheet-body">
+                  <Header />
 
-            <div className="content-layout">
-              <div className="content-layout-row">
-                <Sidebar />
+                  <div className="content-layout">
+                     <div className="content-layout-row">
+                        <Sidebar />
 
-                <div className="layout-cell content">
-                  <div className="post">
-                    <div className="post-body">
-                      <div className="post-inner article">
-                        <div className="pull-right">
-                          <table className="contestlanguages">
-                            <tbody>
-                              <tr>
-                                <td>
-                                  <b>fieldhdr.status :</b>
-                                </td>
-                                <td>
-                                  <span className="label label-success">
-                                    fieldhdr.running
-                                  </span>
-                                  <span className="label label-danger">
-                                    fieldhdr.coming
-                                  </span>
-                                  <span className="label label-default">
-                                    fieldhdr.past
-                                  </span>
-                                </td>
-                                <td>
-                                  <b>fieldhdr.elapsed :</b>
-                                </td>
-                                <td>
-                                  <div
-                                    className="countup"
-                                    data-start-date="${contest.initdate.time}"
-                                    data-end-date="${contest.enddate.time}"
-                                  />
-                                </td>
-                                <td>
-                                  <b>fieldhdr.remaining :</b>
-                                </td>
-                                <td>
-                                  <div
-                                    className="countdown"
-                                    data-date="${contest.enddate.time}"
-                                  />
-                                </td>
+                        <div className="layout-cell content">
+                           <div className="post">
+                              <div className="post-body">
+                                 <div className="post-inner article">
+                                    <div className="pull-right">
+                                       <table className="contestlanguages">
+                                          <tbody>
+                                             <tr>
+                                                <td>
+                                                   <b>fieldhdr.status :</b>
+                                                </td>
+                                                <td>
+                                                   <span className="label label-success">
+                                                      fieldhdr.running
+                                                   </span>
+                                                   <span className="label label-danger">
+                                                      fieldhdr.coming
+                                                   </span>
+                                                   <span className="label label-default">
+                                                      fieldhdr.past
+                                                   </span>
+                                                </td>
+                                                <td>
+                                                   <b>fieldhdr.elapsed :</b>
+                                                </td>
+                                                <td>
+                                                   <div
+                                                      className="countup"
+                                                      data-start-date="${contest.initdate.time}"
+                                                      data-end-date="${contest.enddate.time}"
+                                                   />
+                                                </td>
+                                                <td>
+                                                   <b>fieldhdr.remaining :</b>
+                                                </td>
+                                                <td>
+                                                   <div
+                                                      className="countdown"
+                                                      data-date="${contest.enddate.time}"
+                                                   />
+                                                </td>
 
-                                <td>
-                                  <b>fieldhdr.start :</b>
-                                </td>
-                                <td>
-                                  <b>fieldhdr.end :</b>
-                                </td>
-                              </tr>
-                              <tr>
-                                <td colSpan={6} style={{ textAlign: "center" }}>
-                                  <span className="label label-danger">
-                                    <i className="fa fa-warning" />
-                                    &nbsp; text.deadtime
-                                  </span>
+                                                <td>
+                                                   <b>fieldhdr.start :</b>
+                                                </td>
+                                                <td>
+                                                   <b>fieldhdr.end :</b>
+                                                </td>
+                                             </tr>
+                                             <tr>
+                                                <td
+                                                   colSpan={6}
+                                                   style={{
+                                                      textAlign: "center",
+                                                   }}
+                                                >
+                                                   <span className="label label-danger">
+                                                      <i className="fa fa-warning" />
+                                                      &nbsp; text.deadtime
+                                                   </span>
 
-                                  <span className="label label-primary">
-                                    <i className="fa fa-info-circle" />
-                                    &nbsp; text.frozentime
-                                  </span>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
+                                                   <span className="label label-primary">
+                                                      <i className="fa fa-info-circle" />
+                                                      &nbsp; text.frozentime
+                                                   </span>
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </div>
+                                    <br />
+                                    <div className="clearfix" />
+                                    <a id="header" />
+                                    <Content />
+                                    <div className="pull-right">
+                                       <i className="fa fa-toggle-up fa-lg" />
+                                    </div>
+                                    <div className="cleared" />
+                                 </div>
+                                 <div className="cleared" />
+                              </div>
+                           </div>
                         </div>
-                        <br />
-                        <div className="clearfix" />
-                        <a id="header" />
-                        <Content />
-                        <div className="pull-right">
-                          <i className="fa fa-toggle-up fa-lg" />
-                        </div>
-                        <div className="cleared" />
-                      </div>
-                      <div className="cleared" />
-                    </div>
+                     </div>
                   </div>
-                </div>
-              </div>
+                  <div className="cleared" />
+                  <div className="footer">
+                     <div className="footer-inner">
+                        <div className="footer-text">
+                           <Footer />
+                        </div>
+                     </div>
+                  </div>
+                  <div className="cleared" />
+               </div>
             </div>
             <div className="cleared" />
-            <div className="footer">
-              <div className="footer-inner">
-                <div className="footer-text">
-                  <Footer />
-                </div>
-              </div>
-            </div>
-            <div className="cleared" />
-          </div>
-        </div>
-        <div className="cleared" />
-      </div>
-    </>
-  )
+         </div>
+      </>
+   )
 }
